08ea31b7063900251e42140fbaf560889d40703b
段昊辰 <duanhaochen@126.com>
2020-04-12 23:39:40 +0800

    feat: 完成登陆界面配置文件的选择

--- packages/qqtools/src/pages/Login/index.js
+++ packages/qqtools/src/pages/Login/index.js
@@ -1,26 +1,90 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { createSelector, createStructuredSelector } from 'reselect';
 import { Link } from 'react-router-dom';
-import { Table, Button, Space } from 'antd';
-import { RollbackOutlined as IconRollbackOutlined } from '@ant-design/icons';
+import { Table, Button, Select, Space } from 'antd';
+import { RollbackOutlined as IconRollbackOutlined, QqOutlined as IconQqOutlined } from '@ant-design/icons';
 import style from './index.sass';
+import { queryQqOptions } from './models/models';
+
+/* state */
+const state = createStructuredSelector({
+  qqOptions: createSelector(
+    ({ login: $$login }) => $$login.get('qqOptions'),
+    ($$data) => $$data.toJS()
+  ),
+  loginList: createSelector(
+    ({ login: $$login }) => $$login.get('loginList'),
+    ($$data) => $$data.toJS()
+  )
+});
 
 function Index(props) {
+  const { qqOptions, loginList } = useSelector(state);
+  const dispatch = useDispatch();
+
+  // 渲染select
+  function loginOptionsSelectRender() {
+    return qqOptions.map((item, index) => {
+      return <Select.Option key={ item.id } value={ item.id }>{ item.value.basic.configName }</Select.Option>;
+    });
+  }
+
   // 渲染表格标题
   function titleRender() {
     return (
       <div>
         <Space>
-          <Link to="../">
-            <Button icon={ <IconRollbackOutlined /> }>返回</Button>
-          </Link>
+          <Select className={ style.loginSelect } placeholder="选择登陆的配置文件">
+            { loginOptionsSelectRender() }
+          </Select>
+          <Button.Group>
+            <Button type="primary" icon={ <IconQqOutlined /> }>登陆</Button>
+            <Link to="../">
+              <Button icon={ <IconRollbackOutlined /> }>返回</Button>
+            </Link>
+          </Button.Group>
         </Space>
       </div>
     );
   }
 
+  const columns = [
+    {
+      title: '配置名称',
+      dataIndex: ['value', 'basic', 'configName']
+    },
+    {
+      title: 'QQ号',
+      dataIndex: ['value', 'basic', 'qqNumber']
+    },
+    {
+      title: '群号',
+      dataIndex: ['value', 'basic', 'groupNumber']
+    },
+    {
+      title: '操作',
+      key: 'handle',
+      width: '200px',
+      render: (value, record, index) => {
+        return <Button>登陆</Button>;
+      }
+    }
+  ];
+
+  useEffect(function() {
+    ({ query: { indexName: 'time' } })
+      |> queryQqOptions
+      |> dispatch;
+  }, []);
+
   return (
     <div className={ style.main }>
-      <Table title={ titleRender } />
+      <Table columns={ columns }
+        dataSource={ loginList }
+        title={ titleRender }
+        rowKey="id"
+      />
     </div>
   );
 }
--- packages/qqtools/src/pages/Login/models/models.js
+++ packages/qqtools/src/pages/Login/models/models.js
@@ -1,16 +1,29 @@
 import { createAction, handleActions } from 'redux-actions';
 import { fromJS, List } from 'immutable';
+import { db } from '../../../utils/dbInit';
+import config from '../../../utils/config';
 
+const { objectStore } = config.indexedDB;
+const optionsName = objectStore[0].name;
 const initData = {
-  loginList: [] // 已登陆的列表
+  loginList: [], // 已登陆的列表
+  qqOptions: [] // qq配置列表
 };
 
 export const setLoginList = createAction('login/已登陆列表');
+export const setQqOptions = createAction('login/qq配置列表');
+export const queryQqOptions = db.cursorAction({
+  objectStoreName: optionsName,
+  successAction: setQqOptions
+});
 
 export default {
   login: handleActions({
     [setLoginList]($$state, action) {
       return $$state.set('loginList', List(action.payload));
+    },
+    [setQqOptions]($$state, action) {
+      return $$state.set('qqOptions', List(action.payload.result));
     }
   }, fromJS(initData))
 };
