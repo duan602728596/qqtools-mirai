213767ca6bfd96d362b76882b34b4a8dc0b51322
段昊辰 <duanhaochen@126.com>
2020-04-13 22:47:24 +0800

    pref: 调整表单布局

--- packages/qqtools/src/pages/Options/Add/formSchema.js
+++ packages/qqtools/src/pages/Options/Add/formSchema.js
@@ -1,3 +1,9 @@
+import style from './add.sass';
+
+const $formItemProps = {
+  className: style.formItem
+};
+
 const schema = {
   id: '$root',
   type: 'object',
@@ -12,31 +18,36 @@ const schema = {
           id: '$root/properties/basic/properties/configName',
           type: 'string',
           title: '配置名称',
-          $required: true
+          $required: true,
+          $formItemProps
         },
         qqNumber: {
           id: '$root/properties/basic/properties/qqNumber',
           type: 'number',
           title: '监听QQ号',
-          $required: true
+          $required: true,
+          $formItemProps
         },
         groupNumber: {
           id: '$root/properties/basic/properties/groupNumber',
           type: 'number',
           title: '监听群号',
-          $required: true
+          $required: true,
+          $formItemProps
         },
         port: {
           id: '$root/properties/basic/properties/port',
           type: 'number',
           title: 'socket端口号',
-          $required: true
+          $required: true,
+          $formItemProps
         },
         authKey: {
           id: '$root/properties/basic/properties/authKey',
           type: 'string',
           title: 'authKey',
-          $required: true
+          $required: true,
+          $formItemProps
         }
       }
     }
