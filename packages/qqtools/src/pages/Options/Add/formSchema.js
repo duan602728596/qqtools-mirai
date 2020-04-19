acc3550ab7160e6fc21ad96f8be1cb09dd60d801
段昊辰 <duanhaochen@126.com>
2020-04-19 15:59:56 +0800

    refactor: 添加微博监听配置

--- packages/qqtools/src/pages/Options/Add/formSchema.js
+++ packages/qqtools/src/pages/Options/Add/formSchema.js
@@ -109,6 +109,56 @@ const schema = {
           }
         }
       }
+    },
+    weibo: {
+      id: '$root/properties/weibo',
+      type: 'object',
+      title: '微博监听配置',
+      description: '配置微博监听和推送',
+      properties: {
+        time: {
+          id: '$root/properties/weibo/properties/time',
+          type: 'number',
+          title: '轮询间隔',
+          minimum: 20,
+          $defaultValue: 20
+        },
+        container: {
+          id: '$root/properties/weibo/properties/container',
+          type: 'array',
+          title: '微博监听配置',
+          description: '配置微博监听和推送',
+          maximum: 2,
+          items: {
+            id: '$root/properties/weibo/properties/container/items',
+            type: 'object',
+            title: '微博配置',
+            properties: {
+              name: {
+                id: '$root/properties/weibo/properties/container/items/properties/name',
+                type: 'string',
+                title: '微博名称',
+                $required: true
+              },
+              lfid: {
+                id: '$root/properties/weibo/properties/container/items/properties/lfid',
+                type: 'string',
+                title: '微博lfid',
+                $required: true,
+                $tableColumnHidden: true
+              },
+              use: {
+                id: '$root/properties/weibo/properties/bilibiliLive/items/properties/use',
+                type: 'boolean',
+                title: '开启推送',
+                $defaultValue: true,
+                $componentType: 'switch',
+                $tableColumnHidden: true
+              }
+            }
+          }
+        }
+      }
     }
   }
 };
