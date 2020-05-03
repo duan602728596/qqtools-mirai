d0bfdc6a44c3e5987245634231f629ccd4137562
段昊辰 <duanhaochen@126.com>
2020-05-03 18:14:17 +0800

    fix: 使用id替换time来排序

--- packages/qqtools/src/MiraiQQ/weibo.worker.js
+++ packages/qqtools/src/MiraiQQ/weibo.worker.js
@@ -1,4 +1,3 @@
-import dayjs from 'dayjs';
 import { orderBy } from 'lodash-es';
 import { requestWeiboContainer } from './services/worker';
 
@@ -6,113 +5,6 @@ let id = null;            // { BigInt } 记录时间id
 let config = null;        // 记录配置文件
 let intervalTime = 20000; // 轮询间隔时间
 let timer = null;         // 轮询定时器
-
-/**
- * 根据created_at字段解析时间，可能出现的情况有
- *   刚刚
- *   ss秒前
- *   mm分钟前
- *   HH小时前
- *   昨天 HH:mm
- *   MM-DD
- *   YYYY-MM-DD
- * @param { string } timeStr: 字符串
- */
-function timeStringParse(timeStr) {
-  /**
-   * 字符串为刚刚时，返回当前时间
-   */
-  if (timeStr === '刚刚') {
-    return dayjs().valueOf();
-  }
-
-  /**
-   * 当字符串为秒时，计算时间差
-   */
-  if (/秒(前)?/.test(timeStr)) {
-    const amount = Number(timeStr.match(/[0-9]+/)[0]);
-    const time = dayjs().subtract(amount, 'seconds');
-
-    return time.valueOf();
-  }
-
-  /**
-   * 当字符串为分时，计算时间差
-   */
-  if (/分(钟前)?/.test(timeStr)) {
-    const amount = Number(timeStr.match(/[0-9]+/)[0]);
-    const time = dayjs().subtract(amount, 'minutes');
-
-    return time.valueOf();
-  }
-
-  /**
-   * 当字符串为分时，计算时间差
-   */
-  if (/小?时前?/.test(timeStr)) {
-    const amount = Number(timeStr.match(/[0-9]+/)[0]);
-    const time = dayjs().subtract(amount, 'hours');
-
-    return time.valueOf();
-  }
-
-  /**
-   * 计算昨天的时间
-   */
-  if (/昨天/.test(timeStr)) {
-    const timeArr = timeStr.match(/[0-9]+/g);
-    const time = dayjs();
-
-    const HH = Number(timeArr[0]);
-    const mm = Number(timeArr[1]);
-
-    // 调整时间
-    time.minutes(mm);
-    time.hours(HH);
-    time.subtract(1, 'days');
-
-    return time.valueOf();
-  }
-
-  /**
-   * 计算日期
-   */
-  if (/^[0-9]{1,2}-[0-9]{1,2}$/.test(timeStr)) {
-    const timeArr = timeStr.match(/[0-9]+/g);
-    const time = dayjs();
-
-    const MM = Number(timeArr[0] - 1);
-    const DD = Number(timeArr[1]);
-
-    time.date(DD);
-    time.month(MM);
-
-    return time.valueOf();
-  }
-
-  /**
-   * 日期中包含年份的
-   */
-  if (/^[0-9]{4}-[0-9]{1,2}-[0-9]{1,2}$/.test(timeStr)) {
-    const timeArr = timeStr.match(/[0-9]+/g);
-    const time = dayjs();
-
-    const YYYY = Number(timeArr[0]);
-    const MM = Number(timeArr[1] - 1);
-    const DD = Number(timeArr[2]);
-
-    time.date(DD);
-    time.month(MM);
-    time.year(YYYY);
-
-    return time.valueOf();
-  }
-
-  /**
-   * 不返回任何数据
-   */
-  return undefined;
-}
 
 /**
  * 过滤信息
@@ -124,9 +16,11 @@ function filterCards(cards) {
       return o.card_type === 9 && 'mblog' in o;
     })
     .map((item, index) => {
-      return Object.assign(item, { _time: timeStringParse(item.mblog.created_at) });
+      return Object.assign(item, {
+        _id: BigInt(item.mblog.id)
+      });
     }),
-  ['_time'], ['asc']);
+  ['_id'], ['desc']);
 }
 
 async function handleQueryTimer() {
